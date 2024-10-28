43cb5b356165c05748afeeb39fecf12f04a27a73
Joselito A Guardado <36009999+jaguardado123@users.noreply.github.com>
2024-10-28 14:21:14 -0500

    Update and rename School.java to Main.java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // Assign a value to school_id without creating on object.
+
+        // Create 3 Student objects with unique name and id.
+
+        // Output the student information using studentInfo()
+
+        // Update the school_id
+
+        // Output the student information again to confirm change.
+        
+    }
+}
--- src/School.java
+++
@@ -1,9 +0,0 @@
-public class School {
-
-    public static void main(String[] args) throws Exception {
-        // Test your code here.
-
-    }
-}
-
-// Write your Student class here or create new Student.java file
