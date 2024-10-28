edf733ff22a14b592cd5f70d6af5c55a5202fe93
Joselito A Guardado <36009999+jaguardado123@users.noreply.github.com>
2024-10-28 14:26:43 -0500

    Update StudentTest.java

--- src/StudentTest.java
+++ src/StudentTest.java
@@ -8,26 +8,22 @@ public class StudentTest {
 	
 	@Test
 	public void testing_name_attribute() {
-		Student s1 = new Student();
-		Student s2 = new Student();
-		s1.name = "Bob";
-		s2.name = "Carl";
+		Student s1 = new Student("Bob", "bob@uni.com");
+		Student s2 = new Student("Carl", "carl@uni.com");
 		assertEquals(false, s1.name == s2.name);
 	}
 
 	@Test
 	public void testing_email_attribute() {
-		Student s1 = new Student();
-		Student s2 = new Student();
-		s1.email = "bob@uni.edu";
-		s2.email = "carl@uni.edu";
+		Student s1 = new Student("Bob", "bob@uni.com");
+		Student s2 = new Student("Carl", "carl@uni.com");
 		assertEquals(false, s1.email == s2.email);
 	}
 
 	@Test
 	public void testing_school_id_attribute() {
-		Student s1 = new Student();
-		Student s2 = new Student();
+		Student s1 = new Student("Bob", "bob@uni.com");
+		Student s2 = new Student("Carl", "carl@uni.com");
 		s1.school_id = 123;
 		s2.school_id = 456;
 		assertEquals(true, s1.school_id == s2.school_id);
